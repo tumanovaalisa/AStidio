b1a7f8aca9a73c0d307af56aaa37633ddcca415b
Alisik <Allisatumanova@gmail.com>
2023-10-17 01:06:09 +0300

    Исправление разметки под фрагменты

--- app/src/main/java/com/example/astidio/MainActivity.java
+++ app/src/main/java/com/example/astidio/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.reg_fragment);
     }
 }
