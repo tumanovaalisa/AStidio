c18d722acb2315eff852df730e14924511deb423
79680 <arina.taranukhina@yandex.ru>
2023-12-06 13:15:34 +0300

    Реализован адаптер для отображения расписания и класс занятие.

--- app/src/main/java/com/example/astidio/Timetable.java
+++ app/src/main/java/com/example/astidio/Timetable.java
@@ -1,4 +1,37 @@
 package com.example.astidio;
 
 public class Timetable {
+    private String danceName;
+    private String timeInfo;
+    private int teacherImageResId;
+    private String teacherName;
+    private int availableSeats;
+
+    public Timetable(String danceName, String timeInfo, int teacherImageResId, String teacherName, int availableSeats) {
+        this.danceName = danceName;
+        this.timeInfo = timeInfo;
+        this.teacherImageResId = teacherImageResId;
+        this.teacherName = teacherName;
+        this.availableSeats = availableSeats;
+    }
+
+    public String getDanceName() {
+        return danceName;
+    }
+
+    public String getTimeInfo() {
+        return timeInfo;
+    }
+
+    public int getTeacherImageResId() {
+        return teacherImageResId;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
 }
--- app/src/main/java/com/example/astidio/TimetableAdapter.java
+++ app/src/main/java/com/example/astidio/TimetableAdapter.java
@@ -1,4 +1,80 @@
 package com.example.astidio;
 
-public class TimetableAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
+import java.util.List;
+
+public class TimetableAdapter extends BaseAdapter {
+    private List<Timetable> timetableList;
+    private Context context;
+
+    public TimetableAdapter(Context context, List<Timetable> timetableList) {
+        this.context = context;
+        this.timetableList = timetableList;
+    }
+
+    @Override
+    public int getCount() {
+        return timetableList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return timetableList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            // Inflate the layout for each list item
+            convertView = LayoutInflater.from(context).inflate(R.layout.timetable_item, parent, false);
+
+            // Create a ViewHolder to hold references to your views
+            holder = new ViewHolder();
+            holder.danceNameTextView = convertView.findViewById(R.id.dance_TV);
+            holder.timeInfoTextView = convertView.findViewById(R.id.time_TV);
+            holder.teacherImageView = convertView.findViewById(R.id.img_teacher);
+            holder.teacherNameTextView = convertView.findViewById(R.id.name_TV);
+            holder.availableSeatsTextView = convertView.findViewById(R.id.seats);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        // Get the current timetable entry
+        Timetable timetable = timetableList.get(position);
+
+        // Set data to your views
+        holder.danceNameTextView.setText(timetable.getDanceName());
+        holder.timeInfoTextView.setText(timetable.getTimeInfo());
+        holder.teacherImageView.setImageResource(timetable.getTeacherImageResId());
+        holder.teacherNameTextView.setText(timetable.getTeacherName());
+        holder.availableSeatsTextView.setText(String.valueOf(timetable.getAvailableSeats()));
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView danceNameTextView;
+        TextView timeInfoTextView;
+        ImageView teacherImageView;
+        TextView teacherNameTextView;
+        TextView availableSeatsTextView;
+    }
 }
