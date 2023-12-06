a68fa225f167c2a32ae5c761adf0a965107c1fe5
79680 <arina.taranukhina@yandex.ru>
2023-12-06 13:11:09 +0300

    Реализован адаптер для отображения преподавателей и класс преподаватель.

--- app/src/main/java/com/example/astidio/Teacher.java
+++ app/src/main/java/com/example/astidio/Teacher.java
@@ -1,4 +1,25 @@
 package com.example.astidio;
 
 public class Teacher {
+    private int imageResId;
+    private String name;
+    private String danceType;
+
+    public Teacher(int imageResId, String name, String danceType) {
+        this.imageResId = imageResId;
+        this.name = name;
+        this.danceType = danceType;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDanceType() {
+        return danceType;
+    }
 }
--- app/src/main/java/com/example/astidio/TeacherAdapter.java
+++ app/src/main/java/com/example/astidio/TeacherAdapter.java
@@ -1,4 +1,73 @@
 package com.example.astidio;
 
-public class TeacherAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
+import java.util.List;
+
+public class TeacherAdapter extends BaseAdapter {
+    private List<Teacher> teacherList;
+    private Context context;
+
+    public TeacherAdapter(Context context, List<Teacher> teacherList) {
+        this.context = context;
+        this.teacherList = teacherList;
+    }
+
+    @Override
+    public int getCount() {
+        return teacherList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return teacherList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            // Inflate the layout for each list item
+            convertView = LayoutInflater.from(context).inflate(R.layout.teachers_item, parent, false);
+
+            holder = new ViewHolder();
+            holder.imageView = convertView.findViewById(R.id.img_IV);
+            holder.nameTextView = convertView.findViewById(R.id.name_TV);
+            holder.danceTypeTextView = convertView.findViewById(R.id.dance_TV);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        // Get the current teacher
+        Teacher teacher = teacherList.get(position);
+
+        // Set data to your views
+        holder.imageView.setImageResource(teacher.getImageResId());
+        holder.nameTextView.setText(teacher.getName());
+        holder.danceTypeTextView.setText(teacher.getDanceType());
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        ImageView imageView;
+        TextView nameTextView;
+        TextView danceTypeTextView;
+    }
 }
